24933562d29b3d92851412631122961b83fa425d
Daniil <icet7523@gmail.com>
2023-08-25 20:43:38 +0300

    minor updates Lowercased EventType for sources

--- src/template/event-thumbnail.ts
+++ src/template/event-thumbnail.ts
@@ -12,7 +12,7 @@ const addOffersTemplate = (offers: Offer[]) => (
 const getEventThumbnailTemplate = (state: Point, offers: Offer[], destination: Destination, duration: string) => `<div class="event">
                     <time class="event__date" datetime="${state.dateFrom}">${state.dateFrom.format('MMM DD')}</time>
                     <div class="event__type">
-                      <img class="event__type-icon" width="42" height="42" src="img/icons/${state.type}.png" alt="Event type icon">
+                      <img class="event__type-icon" width="42" height="42" src="img/icons/${state.type.toLowerCase()}.png" alt="Event type icon">
                     </div>
                     <h3 class="event__title">${state.type} ${destination.name}</h3>
                     <div class="event__schedule">
